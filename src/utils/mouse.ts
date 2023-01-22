009a32a5c299ae0ff918344e50bae68fac8a6457
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-01-22 18:44:57 +0300

    feat: add mouse navigation

---
+++ src/utils/mouse.ts
@@ -0,0 +1,84 @@
+import { mouse, down, up, left, right } from '@nut-tree/nut-js';
+import * as stream from 'stream';
+
+class Mouse {
+  wsStream!: stream.Duplex;
+
+  action!: string;
+
+  handler = (ws: stream.Duplex, action: string, value: string[]) => {
+    try {
+      this.wsStream = ws;
+
+      this.action = action;
+
+      const number = parseInt(value[0]!, 10);
+
+      switch (this.action) {
+        case 'up': {
+          this.up(number);
+          break;
+        }
+        case 'right': {
+          this.right(number);
+          break;
+        }
+        case 'down': {
+          this.down(number);
+          break;
+        }
+        case 'left': {
+          this.left(number);
+          break;
+        }
+        case 'position': {
+          this.position();
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    } catch {
+      process.stdout.write('☠️ ERROR\n');
+    }
+  };
+
+  up = async (value: number): Promise<void> => {
+    await mouse.move(up(value));
+    this.send();
+  };
+
+  right = async (value: number): Promise<void> => {
+    await mouse.move(right(value));
+    this.send();
+  };
+
+  down = async (value: number): Promise<void> => {
+    await mouse.move(down(value));
+    this.send();
+  };
+
+  left = async (value: number): Promise<void> => {
+    await mouse.move(left(value));
+    this.send();
+  };
+
+  position = async (): Promise<void> => {
+    const { x, y } = await mouse.getPosition();
+    console.log(` ${x},${y}`);
+
+    const value = ` ${x},${y}`;
+    this.send(value);
+  };
+
+  send = (value: string = ''): void => {
+    const command = `mouse_${this.action}${value}`;
+
+    this.wsStream.write(command, 'utf-8');
+
+    process.stdout.write(`Done: ${command}\n`);
+  };
+}
+
+export default new Mouse();
