d89a1f04aa37c707c24d1c0da40c01a5678882b5
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-01-23 00:51:30 +0300

    feat: add handlerCommand()

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
 import { WebSocketServer, WebSocket, createWebSocketStream } from 'ws';
+
 import { httpServer } from './http_server/index';
+import handlerCommand from './commands/handlerCommand';
 
 const HTTP_PORT = 8181;
 const WS_PORT = 8080;
 
 httpServer.listen(HTTP_PORT);
+process.stdout.write(`🚀 Start static http server on the ${HTTP_PORT} port!\n`);
 
 const wss = new WebSocketServer({ port: WS_PORT });
 
 wss.on('connection', (ws: WebSocket) => {
+  process.stdout.write(`🚀 Start WS server on the ${WS_PORT} port!\n`);
+
   const wsStream = createWebSocketStream(ws, {
     encoding: 'utf8',
     decodeStrings: false,
   });
 
-  wsStream.on('data', async (command: string) => {
-    try {
-      process.stdout.write(command);
-    } catch (err) {
-      process.stdout.write((err as Error)?.message);
-    }
-  });
+  handlerCommand(wsStream);
 
   ws.on('close', () => {
     process.stdout.write('Websocket closed. \n');
@@ -33,6 +32,3 @@ wss.on('connection', (ws: WebSocket) => {
     process.exit(0);
   });
 });
-
-process.stdout.write(`🚀 Start static http server on the ${HTTP_PORT} port!\n`);
-process.stdout.write(`🚀 Start WS server on the ${WS_PORT} port!\n`);
